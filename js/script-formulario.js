5efc75e913aaf0d9b83bfdc11ef1914471599c83
oscar <oscarcornejo10@gmail.com>
2015-09-11 14:30:20 -0300

    Integración y validación de formulario de contacto con el plugin de jquery BootstrapValidator

--- js/script-formulario.js
+++ js/script-formulario.js
@@ -1,54 +1,71 @@
-$(document).ready(function(){
-  console.log("funciona");
-  
-  $("#error").hide();
-  
-  $("#enviarForm").submit(function(event){
-    console.log("funciona");
-
-    var nombre = $("#nombre").val();
-    var correo = $("#correo").val();
-    var asunto = $("#asunto").val();
-    var mensaje = $("#mensaje").val();
-
-    // if(nombre != "" && password !=""){
-    //  // $("#enviado").html("<div class='alert alert-success' role='alert'>Datos enviados</div>");
-    //  //  console.log("El nombre de usuario es: " + nombre);
-    //  //  console.log("Tu password es: " + password);
-    // }
-
-    if(nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)) {
-
-      $(".franja-error").addClass('has-error');
-      $("#error").show('slow');
-      return false;
-
-    }if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(correo)) ) {
-
-      $(".franja-error").addClass('has-error');
-      $("#error").show('slow');
-      return false;
-
-    }if(asunto == null || asunto.length == 0 || /^\s+$/.test(asunto) ) {
-
-      $(".franja-error").addClass('has-error');
-      $("#error").show('slow');
-      return false;
-
-    }if(mensaje == null || mensaje.length == 0 || /^\s+$/.test(mensaje) ) {
-
-      $(".franja-error").addClass('has-error');
-      $("#error").show('slow');
-      return false;
-
-    }else{
-
-      $("#enviado").html("<div class='alert alert-success' role='alert'>Datos enviados</div>");
-
-    }
-
-    return true;
-
-    event.preventDefault();
+$( document ).ready(function() {
+  // Inicio Script Slider
+  $( '#my-slider' ).sliderPro({
+    width: '100%',
+    height: 600,
+    arrows: true,
+    fade: true,
   });
+  // Fin Script Slider
+
+  // Inicio Script BootstrapValidator
+  $('#enviarForm').bootstrapValidator({
+    message: 'Este valor no es válido',
+    live: 'disabled',
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    submitHandler: function(validator, form, submitButton) {
+                // validator is the BootstrapValidator instance
+                // form is the jQuery object present the current form
+                form.find('.alert').html('¡Gracias ' + validator.getFieldElements('nombre').val() + '! Sus datos se enviaron con éxito ').show();
+              },
+              fields: {
+                nombre: {
+                  message: 'Este nombre no es válido',
+                  validators: {
+                    notEmpty: {
+                      message: '* Este campo es requerido'
+                    },
+                    stringLength: {
+                      min: 3,
+                      max: 30,
+                      message: 'Tu nombre debe contener al menos 3 caracteres de largo'
+                    },
+                    regexp: {
+                      regexp: /^[a-zA-Z0-9]+$/,
+                      message: 'Tu nombre debe consistir en letras de la A a la Z y números'
+                    }
+                  }
+                },
+                email: {
+                  validators: {
+                    notEmpty: {
+                      message: '* Este campo es requerido'
+                    },
+                    emailAddress: {
+                      message: 'ingrese una cuenta de correo válida'
+                    }
+                  }
+                },
+                asunto: {
+                  validators: {
+                    notEmpty: {
+                      message: '* Este campo es requerido'
+                    }
+                  }
+                },
+                mensaje: {
+                  validators: {
+                    notEmpty: {
+                      message: '* Este campo es requerido'
+                    }
+                  }
+                }
+              }
+            });
+// Fin Script BootstrapValidator
+
 });
